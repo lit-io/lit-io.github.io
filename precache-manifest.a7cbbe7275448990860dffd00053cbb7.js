c6baa284f100f8975a1312b008699e969134af9c
Mark <mark@remarkablemark.org>
2019-05-13 23:06:20 -0400

    Updates

---
+++ precache-manifest.a7cbbe7275448990860dffd00053cbb7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9fa2003c14329d0620751bcbe6e6f7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17bde65c2e9381a55c7b",
+    "url": "/static/css/main.2f343353.chunk.css"
+  },
+  {
+    "revision": "fd3acf77cf65be54d7f4",
+    "url": "/static/js/2.162a6d0a.chunk.js"
+  },
+  {
+    "revision": "17bde65c2e9381a55c7b",
+    "url": "/static/js/main.41d37b56.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1e60b3bdc74787c2a0ca0df418c82b33",
+    "url": "/static/media/badges.1e60b3bd.png"
+  },
+  {
+    "revision": "bd0693b2a9e491c2e738265394399d8d",
+    "url": "/static/media/chart.bd0693b2.png"
+  },
+  {
+    "revision": "9cbeb146615671470bff5b7967fe761f",
+    "url": "/static/media/daily.9cbeb146.png"
+  }
+]);
